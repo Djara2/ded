e0a40b1f187a99b6f4cc59176c2dd39b4858312d
Dave <jaradavid@protonmail.com>
2024-04-08 21:22:49 -0500

    Kind of got line numbers to display.

--- ded.c
+++ ded.c
@@ -6,10 +6,12 @@
 
 struct line
 {
-	char *contents;
-	unsigned short contents_len;
-	unsigned short contents_capacity;
-}
+	char *data;
+	unsigned short len;
+	unsigned short capacity;
+};
+
+enum modes {NORMAL, TERMINATED};
 
 int main(int argc, char *argv[])
 {
@@ -42,6 +44,8 @@ int main(int argc, char *argv[])
 	unsigned long lines_capacity = 1024;
 	struct line **lines = malloc(sizeof(struct line*) * lines_capacity);
 
+	enum modes mode = NORMAL;
+
 	// read contents into file buffer
 	while((c = fgetc(SOURCE_FILE)) != EOF)
 	{
@@ -69,32 +73,32 @@ int main(int argc, char *argv[])
 		{
 			lines_capacity += 1024;
 
-			lines = realloc(lines, sizeof(char*) * lines_capacity);
-			line_lengths = realloc(line_lengths, sizeof(unsigned short) * lines_capacity);
+			lines = realloc(lines, sizeof(struct line*) * lines_capacity);
 			if(lines == NULL)
 			{
 				fprintf(stderr, "Failed to allocate more memory for the lines buffer.\n");
 				free(file_buffer);
-				free(line_lengths);
-				return 1;
-			}
-			if(line_lengths == NULL)
-			{
-				fprintf(stderr, "Failed to allocate more memory for the line lengths buffer.\n");
-				free(file_buffer);
-				free(lines);
 				return 1;
 			}
 		}
-		// always ensure +1 bytes so that the null terminator and newline can be reinserted at ease.
-		unsigned short required_size = strlen(token) + 1;
 
-		// create memory for line. Record how big that line is in the line_lengths buffer
-		lines[lines_len] = malloc(sizeof(char) * required_size);
-		line_lengths[lines_len] = required_size;
+		// Copy line into lines buffer
+		unsigned short required_size = strlen(token) + 1; // +1 for null terminator
+
+		lines[lines_len] = malloc(sizeof(struct line));  
+		struct line *current_line = lines[lines_len];
+
+		// default allocation style for a line
+		if(required_size >= 512)
+			current_line->data = malloc(sizeof(char) * (required_size + 256));
+		else
+			current_line->data = malloc(sizeof(char) * 512);
+
+		current_line->len = required_size;
+		current_line->capacity = 512;
 
 		// copy line into memory buffer
-		strcpy(lines[lines_len], token);
+		strcpy(current_line->data, token);
 
 		// increment array counters
 		lines_len++;
@@ -105,9 +109,10 @@ int main(int argc, char *argv[])
 	// from here on out, we do not use the file buffer. We modify the lines directly.
 	free(file_buffer);
 
-	// top left corner is origin (0, 0). 
-	unsigned short cursor_x = 0;    // x
-	unsigned long current_line = 0; // y
+	// Because of line numbers, top left corner is NOT the origin (0, 0). 
+	// Instead, the origin is (line_number_max_digits + 1, 0)
+	unsigned short cursor_x = 0;           // x
+	unsigned long current_line_number = 0; // y
 
 	// initialize ncurses
 	initscr();
@@ -115,19 +120,18 @@ int main(int argc, char *argv[])
 	noecho();             // don't echo while we do getch
 	keypad(stdscr, TRUE); // enable special keys
 	
-	// window sizing
+	// Get window sizing
 	int rows, cols;
 	getmaxyx(stdscr, rows, cols); // for some reason, this doesn't need pointers
 
-	// color
+	// Setup ncurses to use colors in the terminal.
 	start_color(); // enable color support
-	init_pair(1, COLOR_YELLOW, A_NORM);
+	init_pair(1, COLOR_YELLOW, A_NORMAL);
 
 	// formula: ceil(log(lines_len, 10)) + 2
 	// - for example, to display line number 1000, you need 4 digits.
 	//   log(1000, 10) = 3, so we add 1 to show all digits.
 	unsigned short line_number_max_digits = (unsigned short) ceil(log10(lines_len)) + 1;
-
 
 	// display file contents
 	for(unsigned long i = 0; i < lines_len; i++)
@@ -137,8 +141,8 @@ int main(int argc, char *argv[])
 		attron(COLOR_PAIR(1));
 
 		// determine how many leading spaces
-		unsigned short current_line_digits = (unsigned short) ceil(log10(i)) + 1;
-		unsigned short difference = line_number_max_digits - current_line_digits;
+		unsigned short current_line_number_digits = (unsigned short) ceil(log10(i)) + 1;
+		unsigned short difference = line_number_max_digits - current_line_number_digits;
 
 		// print leading spaces
 		for(int x = 0; x < difference; x++)
@@ -151,9 +155,9 @@ int main(int argc, char *argv[])
 		attroff(COLOR_PAIR(1));
 		refresh();
 
-		for(unsigned short j = 0; j < strlen(lines[i]); j++)
+		for(unsigned short j = 0; j < lines[i]->len; j++)
 		{
-			char current_char = lines[i][j];
+			char current_char = lines[i]->data[j];
 			printw("%c", current_char);
 			cursor_x++;
 			// if line requires more columns than window has available, go but do not 
@@ -164,6 +168,7 @@ int main(int argc, char *argv[])
 				for(int blankc = 0; blankc < line_number_max_digits; blankc++)
 					printw(" ");
 				printw(" ");
+				cursor_x = line_number_max_digits + 1;
 			}
 		}
 		printw("\n");
@@ -186,9 +191,47 @@ int main(int argc, char *argv[])
 
 		switch(c)
 		{
-			case '\t':
-				printw("\nDo you want autocorrect options?\n> ");
-				break;
+			case KEY_ENTER:
+				current_line++;
+				if(lines_len >= lines_capacity)
+				{
+					lines_capacity += 256;
+					lines = realloc(lines, sizeof(struct line*) * lines_capacity);
+					if(lines == NULL)
+					{
+						fprintf(stderr, "Failed to allocate more memory for the lines buffer after adding newline (lines_len = %lu, lines_capacity = %lu).\n", lines_len, lines_capacity);
+						return 1;
+					}
+				}
+				// if adding new line at end of document, OK
+				if(current_line_number == lines_len - 1)
+				{
+					current_line_number++;
+					lines[lines_len] = malloc(sizeof(struct line));
+				}
+				// otherwise, all following lines must be shifted over (expensive)
+				else
+				{
+					// case: cursor was in the middle of the text so the new line inherits 
+					//       some of the text
+
+					// case: cursor was at the end of the text, so the new line inherits nothing
+				}
+
+				// finally, ensure that new line number is still being displayed.
+				attron(COLOR_PAIR(1));
+				unsigned short current_line_number_digits = (unsigned short) ceil(log10(i)) + 1;
+				unsigned short difference = line_number_max_digits - current_line_number_digits;
+				for(int i = 0; i < difference; i++)
+				{
+					printw(" ");
+				}
+				printw("%lu", current_line_number);
+				printw(" ");
+				attroff(COLOR_PAIR(1)); 
+				cursor_x = line_number_max_digits + 2; 
+				break; 
+
 			case KEY_BACKSPACE: // backspace
 				if(file_buffer_len <= 0)
 					break;
@@ -206,40 +249,60 @@ int main(int argc, char *argv[])
 				file_buffer[file_buffer_len - 1] = '\0';
 				file_buffer_len--;
 				break;
+
 			case KEY_LEFT:
 				// do nothing if at origin
-				if(cursor_x == 0 && current_line == 0)
+				if(cursor_x == line_number_max_digits + 2 && current_line_number == 0)
 					break;
 
 				// go up to previous line and set cursor to end of that line 
-				else if(cursor_x == 0 && current_line != 0)
+				else if(cursor_x == line_number_max_digits + 2 && current_line_number != 0)
 				{
-					if(current_line - 1 < 0)
+					if(current_line_number - 1 < 0)
 						break;
 	
-					current_line--;
-					cursor_x = strlen(lines[current_line]) - 1;
-					move(current_line, cursor_x); // for some reason this uses (y, x)
+					current_line_number--;
+					cursor_x = lines[current_line_number]->len - 1;
+					move(current_line_number, cursor_x); // for some reason this uses (y, x)
 				}
 				else // just move back 1 position
 				{
 					cursor_x--;
-					move(current_line, cursor_x);
+					move(current_line_number, cursor_x);
 				}
 				break;
 
-			case 27:
+			case 27: // if ESC is pressed
 				mode = TERMINATED; 
 				break;
-			default:
-				if(cursor_x < 
+
+			default: // add to line at current index
+				struct line *current_line = lines[current_line_number];
+				if(current_line->len >= current_line->capacity)
 				{
-					
+					current_line->capacity += 256;
+					current_line = realloc(current_line, sizeof(char) * current_line->capacity);
+					if(current_line == NULL)
+					{
+						fprintf(stderr, "Failed to allocate more memory for line %lu.\n", current_line_number);
+						for(int i = 0; i < lines_len; i++)
+						{
+							if(i == current_line_number)
+								continue;
+
+							free(lines[lines_len]->data);
+							free(lines[lines_len]);
+						}
+						free(lines);
+						return 1;
+					}
 				}
-				lines[current_line] = c;
+				
+				current_line->data[cursor_x - (line_number_max_digits - 2)] = c; 
+				cursor_x++;
 				break;
 		}
-		if(c != 7)
+		if(c != KEY_BACKSPACE)
 			printw("%c", c);
 		refresh();
 	}
@@ -248,7 +311,7 @@ int main(int argc, char *argv[])
 	file_buffer[file_buffer_len] = '\0';	
 	SOURCE_FILE = fopen(argv[1], "w");
 	for(unsigned long i = 0; i < lines_len; i++)
-		fprintf(SOURCE_FILE, "%s\n", lines[i]);
+		fprintf(SOURCE_FILE, "%s\n", lines[i]->data);
 
 	fclose(SOURCE_FILE);
 
